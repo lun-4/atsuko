272441808d4a638bcf77ad775305afe25a9fa2e2
Luna <git@l4.pm>
2019-11-20 21:20:34 -0300

    add basic loop

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.debug.warn("All your base are belong to us.\n");
+    var addr = try std.net.Address.parseIp4("0.0.0.0", 8000);
+    var server = std.net.StreamServer.init(.{});
+    defer server.deinit();
+
+    try server.listen(addr);
+
+    while (true) {
+        var sock = try server.accept();
+        defer sock.close();
+
+        std.debug.warn("awoo bitch {}\n", sock.handle);
+
+        var buf = try std.heap.direct_allocator.alloc(u8, 128);
+        const bytecount = try sock.read(buf);
+        var msg = buf[0..bytecount];
+
+        std.debug.warn("got '{}'\n", msg);
+    }
 }
